fda56511e7ad7add2d1b3cbeb439c7793aac5cae
Albert DeFusco <adefusco@anaconda.com>
2024-12-11 14:33:53 -0500

    fake app to provide error handling

--- libs/anaconda-assistant-conda/src/anaconda_assistant_conda/plugin.py
+++ libs/anaconda-assistant-conda/src/anaconda_assistant_conda/plugin.py
@@ -1,7 +1,6 @@
 from typing_extensions import Annotated
 
 import typer
-
 from anaconda_cli_base.cli import ErrorHandledGroup
 from conda import plugins, CondaError
 from conda.exception_handler import ExceptionHandler
@@ -16,20 +15,31 @@ console = Console()
 
 
 def _stream_response(system_message, prompt) -> None:
-    full_text = ""
-    with Live(
-        Markdown(full_text),
-        auto_refresh=False,
-        vertical_overflow="visible",
-        console=console,
-    ) as live:
-        session = ChatSession(system_message=system_message)
-        response = session.chat(prompt, stream=True)
+    fake_app = typer.Typer(
+        help="fake app to support error handling", cls=ErrorHandledGroup
+    )
 
-        for chunk in response:
-            full_text += chunk
-            md = Markdown(full_text)
-            live.update(md, refresh=True)
+    @fake_app.callback(name="chat", invoke_without_command=True, cls=ErrorHandledGroup)
+    def chat(
+        system_message: Annotated[str, typer.Argument],
+        prompt: Annotated[str, typer.Argument],
+    ):
+        full_text = ""
+        with Live(
+            Markdown(full_text),
+            auto_refresh=False,
+            vertical_overflow="visible",
+            console=console,
+        ) as live:
+            session = ChatSession(system_message=system_message)
+            response = session.chat(prompt, stream=True)
+
+            for chunk in response:
+                full_text += chunk
+                md = Markdown(full_text)
+                live.update(md, refresh=True)
+
+    fake_app(args=(system_message, prompt))
 
 
 def error_handler(_) -> None:
