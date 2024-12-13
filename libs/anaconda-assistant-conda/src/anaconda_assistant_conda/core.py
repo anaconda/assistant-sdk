a2a2c0e33929a13658acbbcad3ea07a34b648e48
Albert DeFusco <adefusco@anaconda.com>
2024-12-13 09:51:04 -0500

    error handler without typer

--- libs/anaconda-assistant-conda/src/anaconda_assistant_conda/core.py
+++ libs/anaconda-assistant-conda/src/anaconda_assistant_conda/core.py
@@ -1,37 +1,37 @@
-import typer
+from unittest.mock import patch
+
+from anaconda_cli_base.exceptions import ERROR_HANDLERS
 from anaconda_assistant import ChatSession
-from anaconda_cli_base.cli import ErrorHandledGroup
 from rich.console import Console
 from rich.live import Live
 from rich.markdown import Markdown
-from typing_extensions import Annotated
 
 console = Console()
 
 
-def stream_response(system_message: str, prompt: str) -> None:
-    fake_app = typer.Typer(
-        help="fake app to support error handling", cls=ErrorHandledGroup
-    )
+def stream_response(system_message: str, prompt: str, is_a_tty: bool = True) -> None:
+    full_text = ""
+    with Live(
+        Markdown(full_text),
+        auto_refresh=False,
+        vertical_overflow="visible",
+        console=console,
+    ) as live:
+        with patch("anaconda_cloud_auth.cli.sys") as mocked:
+            mocked.stdout.isatty.return_value = is_a_tty
+            try:
+                session = ChatSession(system_message=system_message)
+                response = session.chat(message=prompt, stream=True)
+            except Exception as e:
+                callback = ERROR_HANDLERS[type(e)]
+                exit_code = callback(e)
+                if exit_code == -1:
+                    session = ChatSession(system_message=system_message)
+                    response = session.chat(prompt, stream=True)
+                else:
+                    return
 
-    @fake_app.callback(name="chat", invoke_without_command=True, cls=ErrorHandledGroup)
-    def chat(
-        system_message: Annotated[str, typer.Argument],
-        prompt: Annotated[str, typer.Argument],
-    ):
-        full_text = ""
-        with Live(
-            Markdown(full_text),
-            auto_refresh=False,
-            vertical_overflow="visible",
-            console=console,
-        ) as live:
-            session = ChatSession(system_message=system_message)
-            response = session.chat(prompt, stream=True)
-
-            for chunk in response:
-                full_text += chunk
-                md = Markdown(full_text)
-                live.update(md, refresh=True)
-
-    fake_app(args=(system_message, prompt))
+        for chunk in response:
+            full_text += chunk
+            md = Markdown(full_text)
+            live.update(md, refresh=True)
