aafe65ab640e1843c647f28d9e5732de09daa087
Albert DeFusco <adefusco@anaconda.com>
2025-02-12 14:04:00 -0500

    allow multiple package recommendations

--- libs/anaconda-assistant-conda/src/anaconda_assistant_conda/config.py
+++ libs/anaconda-assistant-conda/src/anaconda_assistant_conda/config.py
@@ -7,7 +7,7 @@ DEFAULT_SEARCH_SYSTEM_MESSAGE = dedent("""\
 You are the Conda Assistant from Anaconda.
 Your job is to help find useful pip or conda packages that can achieve the outcome requested.
 Do not respond directly to the input.
-You will respond first with the name of the package and the command to install it.
+You will respond first with the name of the packages and then on a new line the command to install them.
 You prefer to use conda and the defaults channel. Do not install from conda-forge unless absolutely necessary.
 You will provide a short description.
 You will provide a single example block of code.
