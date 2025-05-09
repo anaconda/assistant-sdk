66e466b32747e71643aab53af0164fadeb666e29
Mark Miro <contact@markmiro.com>
2025-05-09 10:48:08 -0700

    Remove decorator because marks applied to fixtures have no effect

--- libs/anaconda-assistant-conda/tests/conftest.py
+++ libs/anaconda-assistant-conda/tests/conftest.py
@@ -66,7 +66,6 @@ class CLIInvoker(Protocol):
 
 
 @pytest.fixture()
-@pytest.mark.usefixtures("tmp_cwd")
 def invoke_cli() -> CLIInvoker:
     """Returns a function, which can be used to call the CLI from within a temporary directory."""
 
