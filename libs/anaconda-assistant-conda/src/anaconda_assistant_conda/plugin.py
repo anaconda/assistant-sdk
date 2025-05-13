6a5a2f12fc398ab00c3ac17a8a230c4f4dc3e676
Mark Miro <contact@markmiro.com>
2025-05-13 10:26:57 -0700

    Extract function, fix check against `None`

--- libs/anaconda-assistant-conda/src/anaconda_assistant_conda/plugin.py
+++ libs/anaconda-assistant-conda/src/anaconda_assistant_conda/plugin.py
@@ -12,7 +12,7 @@ from rich.console import Console
 from rich.prompt import Confirm
 
 from .cli import app
-from .config import AssistantCondaConfig
+from .config import AssistantCondaConfig, DebugErrorMode
 from .core import stream_response
 from .debug_config import debug_config, config_command_styled
 from .get_clean_error_report_command import get_clean_error_report_command
@@ -47,13 +47,35 @@ ExceptionHandler._orig_print_conda_exception = (  # type: ignore
     ExceptionHandler._print_conda_exception
 )
 
-interrupt_message_styled = (
-    "\n\n[bold red]Operation canceled by user (Ctrl-C).[/bold red]\n"
-)
+
+def create_message(
+    debug_mode: DebugErrorMode,
+    prompt: str,
+    is_a_tty: bool = True,
+    error: str = "",
+) -> None:
+    # If we don't have a config option, we ask the user
+    if debug_mode == "none":
+        debug_mode = debug_config()
+    if debug_mode == "automatic":
+        stream_response(error, prompt, is_a_tty=is_a_tty)
+    elif debug_mode == "ask":
+        should_debug = Confirm.ask(
+            "[bold]Debug with Anaconda Assistant?[/bold]",
+        )
+        if should_debug:
+            stream_response(error, prompt, is_a_tty=is_a_tty)
+        else:
+            console.print(
+                "\nOK, goodbye! 👋\n"
+                f"To change default behavior, run {config_command_styled}\n"
+            )
 
 
 def error_handler(command: str) -> None:
-    is_a_tty = sys.stdout.isatty()
+    interrupt_message_styled = (
+        "\n\n[bold red]Operation canceled by user (Ctrl-C).[/bold red]\n"
+    )
 
     config = AssistantCondaConfig()
     if config.debug_error_mode == "off":
@@ -82,27 +104,11 @@ def error_handler(command: str) -> None:
             report = self.get_error_report(exc_val, exc_tb)
             command = get_clean_error_report_command(report)
             prompt = f"COMMAND:\n{command}\nMESSAGE:\n{report['error']}"
+            is_a_tty = sys.stdout.isatty()
+            error = config.system_messages.error
 
-            debug_mode = config.debug_error_mode
+            create_message(config.debug_error_mode, prompt, is_a_tty, error)
 
-            # If we don't have a config option, we ask the user
-            if debug_mode == None:
-                debug_mode = debug_config()
-            if debug_mode == "automatic":
-                stream_response(config.system_messages.error, prompt, is_a_tty=is_a_tty)
-            elif debug_mode == "ask":
-                should_debug = Confirm.ask(
-                    "[bold]Debug with Anaconda Assistant?[/bold]",
-                )
-                if should_debug == True:
-                    stream_response(
-                        config.system_messages.error, prompt, is_a_tty=is_a_tty
-                    )
-                else:
-                    console.print(
-                        "\nOK, goodbye! 👋\n"
-                        f"To change default behavior, run {config_command_styled}\n"
-                    )
         # If we're in the conda debug flow, ctrl-c is caught so we don't show stack trace
         except KeyboardInterrupt:
             console.print(interrupt_message_styled)
