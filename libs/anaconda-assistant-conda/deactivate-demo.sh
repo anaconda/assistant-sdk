47682faaa00be0eddb69686fc35f163ec91ab007
Mark Miro <contact@markmiro.com>
2025-05-13 13:25:42 -0700

    Remove unused files

--- libs/anaconda-assistant-conda/deactivate-demo.sh
+++
@@ -1,6 +0,0 @@
-export CONDA_CHANGEPS1=true
-
-if alias conda &>/dev/null; then
-  unalias conda
-fi
-echo "\nDEACTIVATED!\n\nIf you're using bash, run:\nsource ~/.bashrc\n\nIf using zsh, run:\nsource ~/.zshrc\n"
